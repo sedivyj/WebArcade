0549ec8f3fc2cda354a34295b5cd4f2144ab7fcf
Joseph Sedivy <41033622+sedivyj@users.noreply.github.com>
2021-02-04 17:27:34 -0600

    reinstall node packages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
