dc8d89ba3b84b0271c670b518bb32028d7c2303b
vailtra <60304598+vailtra@users.noreply.github.com>
2021-02-09 20:15:27 -0600

    nav bar
    set up a static file system

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require('express')
 const app = express();
 const port = process.env.port || 8080;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
+app.use(express.static(__dirname + '/public'));
 
 
 app.listen(port, () => {
