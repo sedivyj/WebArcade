ecff11778ac6f3ec1ceb650389f6999463d7dc36
Savannah Ford <savannah@coreyford.com>
2021-02-16 08:14:36 -0600

    highscore styling

---
+++ public/js/scores.js
@@ -0,0 +1,13 @@
+$('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+      
+      //chnage count up speed here
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
