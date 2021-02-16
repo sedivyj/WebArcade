daf9483463dd39ed01bc4d478d97dd4655b26ea4
Joseph Sedivy <41033622+sedivyj@users.noreply.github.com>
2021-02-16 15:25:04 -0600

    Update server.js
    Restructured to use configuration file and added a test page for outputting results from simple test query

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
-const express = require('express')
+const express = require('express');
+const port = require('./config.js').app.port
+
+console.log(port)
+
+// DB Module Functions
+const initDb = require('./db').initDb;
+const getDb = require('./db').getDb;
 
 const app = express();
-const port = process.env.port || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
+// Example of using DB
+app.use('/testdb', (req, res)=> {
+    // Get DB Connection Object
+    const db = getDb();
+    // Run query with callback
+    db.query('SELECT * FROM test_table;', (error, results, fields) => {
+        // Check for errors
+        if(error) {
+            res.send("error");
+        } else {
+            res.json(results);
+        }
+    });
+})
 
-app.listen(port, () => {
-    console.log('Server is listening on port ' + port);
-});
+//TODO: Handle error
+// Initializes DB connection and Starts App
+initDb((err) => {
+    app.listen(port, (err) => {
+        try {
+            if (err) throw err;
+            console.log(`Environment: ${process.env.NODE_ENV}`);
+            console.log(`Server is listening on port ${port}`);
+        }
+        catch (err) {
+            console.error(`ERROR IN LISTEN\n${ex.stack}`);
+        }
+    });
+})
