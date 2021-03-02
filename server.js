67dea9c77a20d015f8b873aad9a234969cc4bf47
Larissa Ford <larissa@coreyford.com>
2021-03-02 14:44:12 -0600

    played with high scores page

--- server.js
+++ server.js
@@ -39,6 +39,33 @@ app.use('/testdbAPI', (req, res)=> {
     })
 })
 
+// Example of using DB GET method
+app.use('/getScore', (req, res)=> {
+    console.log("is this getting called?")
+    // Get DB Connection Object
+    const db = getDb();
+    // Getting gameId from header
+    const gameid = req.headers.gameid;
+    // Prepared statement
+    const prepStmt = 'SELECT score.* FROM score WHERE score = ( SELECT MAX(score) FROM score)'
+    // Run query
+    db.query(prepStmt, gameid, (error, result, fields) => {
+        // Error Checking
+        if (error) {
+            console.log("hi")
+            // How to handle err
+            res.json(null);
+
+        } else {
+            console.log("its I")
+            // Create JSON String and return
+            const gameJsonStr = JSON.stringify(result);
+            // telling client-side that it is a JSON response and not reroute
+            res.json(gameJsonStr);
+        }
+    })
+})
+
 // Initializes DB connection and Starts App
 initDb((err) => {
     app.listen(port, (err) => {
