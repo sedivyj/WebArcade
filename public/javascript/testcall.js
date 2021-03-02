c74439dd1faa9108a7c94fe8c1856ece046d9c11
Joseph Sedivy <41033622+sedivyj@users.noreply.github.com>
2021-03-01 21:48:45 -0600

    Updated Test API page
    Updated page to used the updated API tools that run asynchronously

---
+++ public/javascript/testcall.js
@@ -0,0 +1,25 @@
+import { getById } from '../utility/api-tools.js';
+
+// Set up onclick for button when DOM is ready
+$(()=> {
+    $('#btn').on('click', toExpress)
+})
+
+// JQuery DOM Objects
+let textBox = $('#text-box');
+let output = $('#output');
+
+// Function to run onclick
+function toExpress() {
+    const id = textBox.val();
+    output.text('Fetching results...');
+    getById('/testdbAPI', id, success, errfx);
+}
+
+function success(data) {
+    output.text(data);
+}
+
+function errfx(err) {
+    output.text(err);
+}
