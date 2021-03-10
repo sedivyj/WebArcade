74dd0e841a2de7e2d5aca8d26bb516c403602e3d
Seth Schalinske <sethschalinske@gmail.com>
2021-03-09 19:16:04 -0600

    using game span tag now

--- public/javascript/AstroidsGame.js
+++ public/javascript/AstroidsGame.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
     game.load.image('background', 'images/Astroids/StarBackground.png') //Background
