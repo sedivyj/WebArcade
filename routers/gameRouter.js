c401f384f76d62650cb147ea5b1d3c9b42055abf
Joseph Sedivy <41033622+sedivyj@users.noreply.github.com>
2021-03-16 16:15:32 -0500

    Added Cookies
    Cookies now given to user when rating is made and read when doing an overall score

--- routers/gameRouter.js
+++ routers/gameRouter.js
@@ -11,7 +11,7 @@ let router = express.Router()
 // router.use(express.static('public')) // Could be used for getting game assets
 
 // API for getting the high score for a game
-router.use('/getHighScore', (req, res)=> {
+router.use('/getHighScore', (req, res) => {
     console.log("is this getting called?")
     // Get DB Connection Object
     const db = getDb();
@@ -36,49 +36,84 @@ router.use('/getHighScore', (req, res)=> {
 })
 
 // API for getting overall positive rating of a game
-router.use('/getOveralRating/:gameId', (req, res)=> {
-    // Get DB Connection Object
-    const db = getDb();
-    // Getting gameId from header
-    const body = req.body;
-    console.log(body)
-    res.end()
-    // // Prepared statement
-    // const prepStmt = 'SELECT (SUM(positive) / count(*) * 100.0) as positive_percent FROM rating GROUP BY fk_gameid HAVING fk_gameid=?;'
-    // // Run query
-    // db.query(prepStmt, gameid, (error, result, fields) => {
-    //     // Error Checking
-    //     if (error) {
-    //         // console.log(error)
-    //         return res.status(500).json(null).end();
+// Returns the overall positive rating and if the user
+// has ratedthe game before
+router.use('/getOveralRating/:gameId', (req, res) => {
 
-    //     } else {
-    //         // console.log(result)
-    //         return res.status(200).json(result).end();
-    //     }
-    // })
+    // Getting gameId from header
+    // // Getting gameId from header
+    const gameid = req.params.gameId;
+    if (gameid) {
+        // Get DB Connection Object
+        const db = getDb();
+        // // Prepared statement
+        const prepStmt = 'SELECT (SUM(positive) / count(*) * 100.0) as positive_percent FROM rating GROUP BY fk_gameid HAVING fk_gameid=?;'
+        // Run query
+        db.query(prepStmt, gameid, (error, result, fields) => {
+            // Error Checking
+            if (error) {
+                // console.log(error)
+                return res.status(500).json(null).end()
+
+            } else {
+                // Query was successful, but if empty -> There were no ratings!
+                const percent = (result[0]) ? result[0].positive_percent : undefined
+                const cookie = req.cookies[`game_${gameid}_rated`] // get cookie
+                let wasPositiveRating = undefined
+                console.log(cookie)
+                if(cookie) {
+                    wasPositiveRating = cookie.wasPositive
+                }
+                const response = {
+                    positivePercent: percent, // should only have 1 or none response
+                    
+                    wasPositive: wasPositiveRating
+                }
+                return res.status(200).json(response).end()
+            }
+        })
+    } else {
+        return res.status(400).json({status: "Bad request"}).end()
+    }
 })
 
 // API for submitting a rating to a game
-router.use('/rateGame', (req, res)=> {
-    // Get DB Connection Object
-    const db = getDb();
-    // Getting gameId from header
-    const gameid = req.params.gameId;
-    // Prepared statement
-    const prepStmt = 'SELECT (SUM(positive) / count(*) * 100.0) as positive_percent FROM rating GROUP BY fk_gameid HAVING fk_gameid=?;'
-    // Run query
-    db.query(prepStmt, gameid, (error, result, fields) => {
-        // Error Checking
-        if (error) {
-            // console.log(error)
-            return res.status(500).json(null).end();
-
+router.use('/rateGame', (req, res) => {
+    // Getting body from request
+    const body = req.body
+    if (body) {
+        const gameId = body.gameId
+        // Check cookie
+        const ratedCookie = req.cookies[`game_${gameId}_rated`]
+        if (ratedCookie) {
+            const errorMessage = new Error("You've rated this game before")
+            return res.status(400).json(errorMessage).end()
         } else {
-            // console.log(result)
-            return res.status(200).json(result).end();
+            const positive = body.positive
+            const ratingId = null
+            // Get a current timestamp in format for MySQL DB
+            const dateTime = new Date().toISOString().slice(0, 19).replace('T', ' ')
+            // Get DB Connection Object
+            const db = getDb();
+            // Prepared statement
+            const prepStmt = 'INSERT INTO rating (ratingid, fk_gameid, positive, timestamp) VALUES (?,?,?,?);'
+            // Run query
+            db.query(prepStmt, [ratingId, gameId, positive, dateTime], (error, result, fields) => {
+                // Error Checking
+                if (error) {
+                    return res.status(500).json(null).end();
+                } else {
+                    res.cookie(`game_${gameId}_rated`, {
+                        gameRated: true,
+                        wasPositive: positive
+                    })
+                    return res.status(200).json(result).end();
+                }
+            })
         }
-    })
+    } else {
+        return res.status(400).json("Body missing from request").end()
+    }
 })
 
 module.exports = router
