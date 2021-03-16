fc5f23fb3d1ca7233f7401464eba7847dcbf9e46
Joseph Sedivy <41033622+sedivyj@users.noreply.github.com>
2021-03-16 16:17:26 -0500

    Cleaned Code and Button Styling
    Buttons now disabled and styled depending on response from server.

--- public/javascript/ratingButtons.js
+++ public/javascript/ratingButtons.js
@@ -1,46 +1,102 @@
 import { getById, postData } from '../utility/api-tools.js';
-// TODO: Set up functionality for GETTING overall rating of game
 
 /**
  * Sets up button click listeners when document is ready
  */
 $(() => {
     getById('/game/getOveralRating', 1, overallRatingSuccess, overallRatingFail)
-    
-    const downVoteButton = $('#vote-down').click(downVote)
-    const upVoteButton = $('#vote-up').click(upVote)
+    // Set up buttons' onClick listeners
+    $('#vote-down').click(downVote)
+    $('#vote-up').click(upVote)
 })
 
+/**
+ * Callback function that sets the percentage in HTML page and CSS styling
+ * for the percentage text and buttons depending on the data returned
+ * @param {object} data 
+ */
 function overallRatingSuccess (data) {
-    let percentSpan = $('#overall-rating')
     // Undefined Check
     if (data) {
-        // Array check
-        if(data.length > 0) {
-            let percentage = data[0].positive_percent
-            percentSpan.text(`${parseFloat(percentage).toFixed(2)}%`)
+        let percentSpan = $('#overall-rating')
+        // Check if percentage set and update color of text
+        if(data.positivePercent) {
+            let percentage = data.positivePercent
+            let textColor = undefined
+            if (percentage >= 70.0) {
+                textColor = 'green'
+            } else if (percentage >= 50.0 ) {
+                textColor = 'yellow'
+            } else {
+                textColor = 'red'
+            }
+            percentSpan.text(`${parseFloat(percentage).toFixed(2)}%`).css('color', textColor)
         } else {
-            percentSpan.text(`No Ratings!`)
+            // Percent wasn't defined -> There were no ratings
+            percentSpan.text(`No Ratings!`).css('color', 'white')
+        }
+        // Check if game was previously rated
+        if(!(data.wasPositive === undefined)) {
+            styleButtons(data.wasPositive)
         }
     } else {
         percentSpan.text('ERROR')
     }
 }
 
+/**
+ * Displays an alert of an error 
+ * @param {string} error 
+ */
 function overallRatingFail (error) {
     console.error(error);
 }
 
+// Setting up callback functions for each button
+const downVote = () => vote(false)
+const upVote = () => vote(true)
 
 /**
  * Function to call when a vote happens
  * @param {boolean} isPositve whether or not the player liked the game
  */
 function vote(isPositve) {
-    let rating = (isPositve) ? "LIKED" : "HATED";
-    console.log(`The player ${rating} this game!`);
+    const ratingData = {
+        gameId: 1,
+        positive: isPositve
+    }
+
+    postData('/game/rateGame', ratingData, voteSuccess, voteFail)
 }
 
-// Setting up callback functions for each button
-const downVote = () => vote(false)
-const upVote = () => vote(true)
+/**
+ * Handles when rating is a success
+ * Disables buttons
+ * @param {*} response 
+ */
+function voteSuccess(response) {
+    styleButtons(response.wasPositive)
+}
+
+function styleButtons(wasPositive) {
+    let downVoteButton = $('#vote-down')
+    let upVoteButton = $('#vote-up')
+    downVoteButton.prop('disabled', true)
+    upVoteButton.prop('disabled', true)
+    // Change button colors depending on the rating
+    if(wasPositive) {
+        upVoteButton.css('background-color', 'green')
+        downVoteButton.css('background-color', 'grey')
+    } else {
+        upVoteButton.css('background-color', 'lightgreen')
+        downVoteButton.css('background-color', 'grey')
+    }
+}
+
+//
+/**
+ * Alerts user that they've rated the game before
+ */
+function voteFail() {
+    alert("You've rated this before!")
+}
