0d1a94a072bb61ee0726851c031d5c9b9fd9c3b4
Larissa Ford <larissa@coreyford.com>
2021-03-18 11:38:04 -0500

    combined score methods in scoreController

--- sql/scoreController.js
+++ sql/scoreController.js
@@ -1,45 +1,22 @@
 // DB Module Functions
 const getDb = require('../db').getDb;
 
-function getGameHighScore (id) {
-
+function getGameHighScore (id, limit) {
     const db = getDb();
 
     return new Promise((resolve, reject) => {
 
-        const prepStmt = 'SELECT score.* FROM score WHERE score = ( SELECT MAX(score) FROM score)'
+        const prepStmt = 'select * from score where fk_gameid=? order by score desc limit ?'
         // Run query
-        db.query(prepStmt, id, (error, result, fields) => {
+        db.query(prepStmt, [id, parseInt(limit)], (error, result,  fields) => {
             // Error Checking
             if (error) { 
                 console.log("is this where its breaking?")
+                console.log(error)
                 return reject(error) 
             }
             // Was the game found
             if (!result) { return reject(new Error('Invalid id for game')) }
-            
-            // Return the email and row data (without password) merged
-            return resolve(result)
-        })
-    })
-}
-
-function getGameLeaderboard (id) {
-
-    const db = getDb();
-
-    return new Promise((resolve, reject) => {
-
-        const prepStmt = 'select * from score where fk_gameid=1 order by score desc limit 5'
-        // Run query
-        db.query(prepStmt, id, (error, result, fields) => {
-            // Error Checking
-            if (error) { 
-                return reject(error) 
-            }
-            // Was the game found
-            if (!result) { return reject(new Error('Invalid id for game')) }
-            
             // Return the email and row data (without password) merged
             return resolve(result)
         })
