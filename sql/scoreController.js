737ee31c3cf0ce1a3d95387c3c88d9cc01627017
Larissa Ford <larissa@coreyford.com>
2021-03-18 10:59:25 -0500

    added getScores function to score controller

--- sql/scoreController.js
+++ sql/scoreController.js
@@ -24,4 +24,28 @@ function getGameHighScore (id) {
     })
 }
 
+function getGameLeaderboard (id) {
+
+    const db = getDb();
+
+    return new Promise((resolve, reject) => {
+
+        const prepStmt = 'select * from score where fk_gameid=1 order by score desc limit 5'
+        // Run query
+        db.query(prepStmt, id, (error, result, fields) => {
+            // Error Checking
+            if (error) { 
+                console.log("is this where its breaking?")
+                return reject(error) 
+            }
+            // Was the game found
+            if (!result) { return reject(new Error('Invalid id for game')) }
+            
+            // Return the email and row data (without password) merged
+            return resolve(result)
+        })
+    })
+}
+
+
 module.exports = {getGameHighScore}
